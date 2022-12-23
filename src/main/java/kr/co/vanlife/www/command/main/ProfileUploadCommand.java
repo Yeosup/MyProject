88adda0c6fefca8c582ecb45c52006644c7eada5
ysshin <yaerang54@gmail.com>
2022-12-23 10:10:14 +0900

    savePath_ubuntu

--- src/main/java/kr/co/vanlife/www/command/main/ProfileUploadCommand.java
+++ src/main/java/kr/co/vanlife/www/command/main/ProfileUploadCommand.java
@@ -28,7 +28,8 @@ public class ProfileUploadCommand implements Command {
 		FriendDTO dtoM = new FriendDTO();
 		MultipartRequest multi = null;
 		int fileMaxSize = 10 * 1024 * 1024;
-		String savePath = request.getSession().getServletContext().getRealPath("/upload");
+//		String savePath = request.getSession().getServletContext().getRealPath("/upload");
+		String savePath = "root@ip-172-31-38-67:/home/ubuntu/upload";
 		String file = "";
 		String oriFile = "";
 		try {
